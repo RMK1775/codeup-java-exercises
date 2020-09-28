5811fd75c1c8e3372e3aaad1051668be5f2c1262
RMKendall <ryan.morris.kendall@gmail.com>
2020-09-28 14:15:20 -0500

    Complete the syntax, types and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,53 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.print("Hello,");
         System.out.print(" World!");
+
+        int myFavoriteNumber = 1775;
+        System.out.println("\n" +myFavoriteNumber);
+
+        String myString = "I ride a Harley.";
+        System.out.println(myString);
+        //long myNumber = 123;
+        //System.out.println(myNumber);
+        float myNumber = (float) 3.14;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+        // Result: 5, 6
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+        // Result: 6, 6
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+        // ^ Can't convert
+
+        //int three = (int) "three";
+        // ^ Can't convert String to int
+
+//        int x = 4;
+//        x = x + 5; Result: 9
+//        x += 5; Result also 9
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x; Result: 12
+//        y *= x; Result also 12
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y; Result: 5
+//        x /= y; Result also 5
+//        System.out.println(x);
+//        y = y - x; Result: -8
+//        y -= x; Result also -8
+//        System.out.println(y);
+
+
+
     }
 }
