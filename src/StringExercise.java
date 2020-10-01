1ed0222d42b1fd7bdeff2cb45cdfde5a8c8b6c5d
RMKendall <ryan.morris.kendall@gmail.com>
2020-10-01 09:07:40 -0500

    Complete the Java String exercises

---
+++ src/StringExercise.java
@@ -0,0 +1,16 @@
+public class StringExercise {
+    public static void main(String[] args) {
+//    String Basics
+        String floydLyrics = "We don't need no education.\nWe don't need no thought control.";
+        System.out.println(floydLyrics);
+
+        String escapeQuotes = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(escapeQuotes);
+
+        String backSlash = "In Windows, the main drive is usually C:\\";
+        System.out.println(backSlash);
+
+        String slashHappy = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(slashHappy);
+    }
+}
