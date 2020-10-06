88305e626ec0afe330738dd0f910c1a8d342a70a
RMKendall <ryan.morris.kendall@gmail.com>
2020-10-06 11:13:10 -0500

    Completes Java Input exercise
    - utilities package created with basic toolset for strings, ints, doubles and yes, no booleans

---
+++ src/util/InputTest.java
@@ -0,0 +1,14 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input scan = new Input();
+        // System.out.println(scan.getString());
+        // System.out.println(scan.yesNo());
+        // System.out.println(scan.getInt(19,11));
+        // System.out.println(scan.getInt());
+        //System.out.println(scan.getDouble(7.62, 5.56));
+        // System.out.println(scan.getDouble());
+    }
+}
+
