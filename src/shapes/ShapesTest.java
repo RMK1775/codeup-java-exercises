1f1d3ca52921b89526616177c2a40a87c793ca64
RMKendall <ryan.morris.kendall@gmail.com>
2020-10-09 13:29:31 -0500

    Complete the Java exercises on Interfaces and Abstract Classes

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,10 +2,22 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
+        Measurable myShape;
+
+        myShape =new Rectangle(10,5);
+        System.out.println("Rectangle area = " + myShape.getArea() + "\n");
+        System.out.println("Rectangle perimeter = " + myShape.getPerimeter() + "\n");
+        System.out.println("******");
+
+        myShape = new Square(5);
+        System.out.println("Square area = " + myShape.getArea() + "\n");
+        System.out.println("Square perimeter = " + myShape.getPerimeter() + "\n");
+        /* --Commented out for Interface and Abstract Exercises
         Rectangle box1 = new Rectangle(5, 4);
         Rectangle box2 = new Square(5);
 
         System.out.printf("Box 1 has:\n\tan area of %.2g and\n\ta perimeter of %.2g\n", box1.getArea(), box1.getPerimeter());
         System.out.printf("Box 2 has:\n\tan area of %.2g and\n\ta perimeter of %.2g",box2.getArea(), box2.getPerimeter() );
+        */
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,21 +1,51 @@
 package shapes;
 
-public class Square extends Rectangle {
-
-    public Square(double aSide) {
-        super(aSide, aSide);
+public class Square extends Quadrilateral {
+    public Square(double aSide){
+        super(aSide,aSide);
     }
 
-    public double getSide(){
-        return this.length;
+    public void setSide(double aSide){
+        this.length = aSide;
+        this.width = aSide;
     }
 
-    public double getArea(){
-        return Math.pow(this.getSide(),2);
+    @Override
+    public void setLength(double aSide) {
+        this.setSide(aSide);
     }
 
-    public double getPerimeter(){
-        return this.getSide()*4;
+    @Override
+    public void setWidth(double aSide) {
+        this.setSide(aSide);
     }
+
+    @Override
+    public double getPerimeter() {
+        return this.length * 4;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.pow(this.length, 2);
+    }
+
+    /* --Original code before Interface and Abstract Lesson
+        public Square(double aSide) {
+            super(aSide, aSide);
+        }
+
+        public double getSide(){
+            return this.length;
+        }
+
+        public double getArea(){
+            return Math.pow(this.getSide(),2);
+        }
+
+        public double getPerimeter(){
+            return this.getSide()*4;
+    }
+    */
 
 }
