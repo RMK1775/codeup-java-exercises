c8ee21657c25f7c1bf56a816649a11e779be791d
RMKendall <ryan.morris.kendall@gmail.com>
2020-10-15 14:10:13 -0500

    Complete the Java Exceptions exercise
    - utilized getString  and valueOf method for all integer and double methods
    - added exception handlers to all integer and double methods

--- src/util/Input.java
+++ src/util/Input.java
@@ -22,12 +22,12 @@ public class Input {
 
     public int getInt(int min, int max) {
         int input;
-        boolean exceptionFound = false;
+        boolean exceptionFound;
         boolean outOfBounds = false;
         do {
             exceptionFound = false;
             System.out.println("Enter a number between " + min + " and " + max + ": ");
-            input = this.scanner.nextInt();
+            input = Integer.valueOf(getString());
             try {
                 if (input < min || input > max) {
                     System.out.println("Number outside the given parameters.");
@@ -48,38 +48,54 @@ public class Input {
     }
 
     public int getInt() {
-        System.out.println("Input a number: ");
-        return this.scanner.nextInt();
+        System.out.print("Input a number: ");
+        int input = 0;
+        try {
+            input = Integer.valueOf(getString());
+        }catch (Exception e){
+            System.out.println("That is not a valid number.\n");
+            getInt();
+        }
+        return input;
     }
 
     public int getInt(String prompt){
         System.out.print(prompt);
-        return this.scanner.nextInt();
+        int input = 0;
+        try {
+            input = Integer.valueOf(getString());
+        }catch (Exception e){
+            System.out.println("That is not a valid number.\n");
+            getInt();
+        }
+        return input;
     }
 
     public double getDouble(double min, double max) {
         double input = 0.0;
-        boolean outOfBounds = false;
-        do {
-            outOfBounds = false;
-            System.out.println("Enter a number between " + min + " and " + max + ".");
+        System.out.println("Enter a number between " + min + " and " + max + ".");
             try {
-                input = this.scanner.nextDouble();
-            } catch (NumberFormatException e) {
-                System.out.println("That is not a double");
-                outOfBounds = true;
+                input = Double.valueOf(getString());
+            } catch (Exception e) {
+                System.out.println("That is not a valid number.");
+                getDouble(min, max);
             }
             if (input < min || input > max) {
                 System.out.println("You've entered a value outside the given parameters.");
-                System.out.println();
+                getDouble(min, max);
             }
-        } while (outOfBounds || input < min || input > max);
-
         return input;
     }
 
     public double getDouble() {
         System.out.println("Input a number: ");
-        return this.scanner.nextDouble();
+        double input = 0.0;
+        try {
+            input = Double.valueOf(getString());
+        } catch (Exception e) {
+            System.out.println("That is not a valid number.");
+            getDouble();
+        }
+        return input;
     }
 }
